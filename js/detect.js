3321a2bad36f1277588990f2568b6b8aadfa7b23
Lukas Greblikas <grebliux@gmail.com>
2014-10-22 03:02:34 +0100

    Animation is on fire. Much animation, much colour and much canvas. Yippy - it even works.

--- js/detect.js
+++ js/detect.js
@@ -3,13 +3,18 @@ var suggestionBox = $('\
 		<img src="' + chrome.extension.getURL("images/lock-16.png") + '" />\
 		<div>You should use longer password</div>\
 	</div>\
+  <canvas id="imageReminder" width="557" height="195" ></canvas>\
 ');
 
 var keyboard = new Array();
-keyboard[0] = [['`', '¬'], ['1', '!'], ['2', '"'], ['3', '£'], ['4', '$'], ['5', '%'], ['6', '^'], ['7', '&'], ['8', '*'], ['9', '('],  ['0', ')'], ['-', '_'], ['=', '+']]
-keyboard[1] = [undefined,   'q', 'w', 'e', 'r', 't', 'y', 'u', 'i',       'o',         'p',        ['[', '{'], [']', '}']]
-keyboard[2] = [undefined,   'a', 's', 'd', 'f', 'g', 'h', 'j', 'k',       'l',         [';', ':'], ["'", '@'], ['#', '~']]
-keyboard[3] = [['\\', '|'], 'z', 'x', 'c', 'v', 'b', 'n', 'm', [',', '<'], ['.', '>'], ['/', '?'], undefined,  undefined]
+var COORD_STEP = 36;
+var Y_START = 26;
+var WIDTH = 25;
+
+keyboard[0] = {x_start: 26, keys: [['`', '¬'], ['1', '!'], ['2', '"'], ['3', '£'], ['4', '$'], ['5', '%'], ['6', '^'], ['7', '&'], ['8', '*'], ['9', '('],  ['0', ')'], ['-', '_'], ['=', '+']]}
+keyboard[1] = {x_start: 42, keys: [undefined, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i',       'o',         'p',        ['[', '{'], [']', '}']]}
+keyboard[2] = {x_start: 54, keys: [undefined, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k',       'l',         [';', ':'], ["'", '@'], ['#', '~']]}
+keyboard[3] = {x_start: 74, keys: [['\\', '|'], 'z', 'x', 'c', 'v', 'b', 'n', 'm', [',', '<'], ['.', '>'], ['/', '?'], undefined,  undefined]}
 
 function isRegistrationPage(){
   return $('input[type="password"]').length > 0;
@@ -24,26 +29,28 @@ function generateBlizzard(){
   var row = 1;
   var col = 0;
   var sugg = "";
+  var line = [];
   
-  while (keyboard[row][col] === undefined) {
+  while (keyboard[row].keys[col] === undefined) {
     row = randomItem(keyboard).index;
-    col = randomItem(keyboard[row]).index;
+    col = randomItem(keyboard[row].keys).index;
   }
   
   while(sugg.length < 8){
+    //animation
+    line.push({ x: col*COORD_STEP + keyboard[row].x_start, y: row*COORD_STEP+Y_START})
     var m = Math.round(Math.random());
-    if($.isArray(keyboard[row][col])) 
-      sugg += keyboard[row][col][m];
+    if($.isArray(keyboard[row].keys[col])) 
+      sugg += keyboard[row].keys[col][m];
     else
       if(m == 0)
-        sugg += keyboard[row][col];
+        sugg += keyboard[row].keys[col];
       else
-        sugg += keyboard[row][col].toUpperCase();
+        sugg += keyboard[row].keys[col].toUpperCase();
     
     var next = { row: -1, col: -1 };
-    while(
-          keyboard[next.row] === undefined
-          || keyboard[next.row][next.col] === undefined) {
+    while( keyboard[next.row] === undefined
+           || keyboard[next.row].keys[next.col] === undefined) {
       
       neighbours = [
         /*{ row: row-1, col: col-1 },*/   { row: row-1, col: col },   { row: row-1, col: col+1 },
@@ -55,18 +62,45 @@ function generateBlizzard(){
     row = next.row;
     col = next.col; 
   }
-  return sugg;
+  return {line: line, sugg: sugg };
 }
 
 $(document).ready(function(){
-  for (var i = 0; i < 100; i++) {
-    console.log(generateBlizzard());
-  }
+  $('input[type="password"]').first().after(suggestionBox);
+  var psw = generateBlizzard();
+  console.log(psw.sugg);
+  var canvas = document.getElementById("imageReminder");
+  var ctx = canvas.getContext("2d");
+  ctx.globalAlpha=0.2;
+  /*
+  for (var i=0; i < psw.line.length; i++) {
+    ctx.lineTo(psw.line[i].x, psw.line[i].y);
+    ctx.moveTo(psw.line[i].x, psw.line[i].y);
+  }*/
+  var i = 0;
+  setInterval(function() {
+    ctx.lineTo(psw.line[i].x, psw.line[i].y);
+    ctx.moveTo(psw.line[i].x, psw.line[i].y);
+    ctx.fillStyle="#FF0000";
+    ctx.fillRect(psw.line[i].x - WIDTH/2.0, psw.line[i].y - WIDTH/2.0, WIDTH, WIDTH);
+    ctx.stroke();
+    if (i < psw.line.length - 1) 
+      i++;
+    else {
+      i = 0;
+      ctx.beginPath();
+      ctx.clearRect(0,0, 9999, 9999);
+    }
+  }, 2000);
+  //ctx.stroke();
 	if(isRegistrationPage()) 
 		$('input[type="password"]').keyup(function() {
 			$(suggestionBox).remove();
 			if($(this).val().length < 8) {
 				$(this).after(suggestionBox);
+        $("#imageReminder").mousemove(function( ev ){
+          console.log(ev.offsetX + ' ' + ev.offsetY);
+        });
 			}
 		});
 });
